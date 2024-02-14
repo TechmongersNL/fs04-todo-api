b39739101a1636b320405e266f60259ca150c0b0
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2024-02-14 16:00:04 +0100

    feat: Add Lists and Tasks models

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from .database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+class List(Base):
+    __tablename__ = "lists"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String)
+
+    tasks = relationship("Task", back_populates="list")
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    title = Column(String)
+    list_id = Column(Integer, ForeignKey("lists.id"))
+
+    list = relationship("List", back_populates="tasks")
